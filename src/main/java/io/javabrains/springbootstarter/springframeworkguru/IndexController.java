b758c500f9de3387ead2d9598ac59fa5144ef6f3
Dr <ioan12@protonmail.com>
2022-02-10 14:20:27 +0200

    @RequestMapping with @RequestParam

--- src/main/java/io/javabrains/springbootstarter/springframeworkguru/IndexController.java
+++ src/main/java/io/javabrains/springbootstarter/springframeworkguru/IndexController.java
@@ -1,6 +1,7 @@
 package io.javabrains.springbootstarter.springframeworkguru;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +22,33 @@ public class IndexController {
         @RequestMapping(value={"", "/page", "page*","view/*,**/msg"})
         String indexMultipleMapping(){
             return "Hello from index multiple mapping.";
+        }
+        @RequestMapping(value="/id")
+        String getIdByValue(@RequestParam("id1") String personId){
+            System.out.println("Id is "+personId);
+            return "Get ID from query string of URL with value element";
+        }
+        @RequestMapping( "/personId")
+        String getId(@RequestParam String pId){
+            System.out.println("ID is "+pId);
+            return "Get ID from query string of URL without value element";
+        }
+        @RequestMapping("/name")
+        String getName(@RequestParam(value="nume", required = false,defaultValue = "Gigi") String personName){
+            if(personName == null){
+                System.out.println("nume este "+personName+" care este de fapt default");
+                return "Required element of request param null";
+            } else {
+                System.out.println("nume este "+personName);
+                return "Required element of request param";
+            }
+
+
     }
 
 }
+
+
+
 
 
